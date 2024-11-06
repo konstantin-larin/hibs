91a1b096e7c71aa93969a88806eca64a1162aa0d
Константин <konstantin.larin.33@gmail.com>
2024-11-06 22:04:13 +0300

    init

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import {useState} from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.scss'
+import {BrowserRouter, Route, Routes} from "react-router-dom";
+import Home from "./pages/Home.jsx";
+
+function App() {
+    return (
+        <BrowserRouter basename={'/hibs/'}>
+            <Routes>
+                <Route path={'/'} element={<Home/>}></Route>
+            </Routes>
+        </BrowserRouter>
+    )
+}
+
+export default App
