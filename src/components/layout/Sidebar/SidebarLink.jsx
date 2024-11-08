c27e566299194e7d216310cf1d5ce4591a8ce141
Константин <konstantin.larin.33@gmail.com>
2024-11-08 20:34:52 +0300

    small fixes

--- src/components/layout/Sidebar/SidebarLink.jsx
+++ src/components/layout/Sidebar/SidebarLink.jsx
@@ -1,13 +1,20 @@
 import "@layout/style.scss";
 import {Link, useLocation} from "react-router-dom";
 import Text from "@common/Text/Text.jsx";
+import {usePreferences} from "@contexts/PreferencesContext.jsx";
 
 export default function SidebarLink({to, img, label}) {
+    const {isMobile, setSidebarIsClosed} = usePreferences();
     const Img = img;
     const location = useLocation();
 
+    function handleOnClick(){
+        if(isMobile){
+            setSidebarIsClosed(true);
+        }
+    }
     return (
-        <Link to={to} className={'sidebar__link ' + (location.pathname === to ? "sidebar__link_active" : '')}>
+        <Link to={to} onClick={handleOnClick} className={'sidebar__link ' + (location.pathname === to ? "sidebar__link_active" : '')}>
             <div className={'sidebar__link-img'}>
                 <Img></Img>
             </div>
